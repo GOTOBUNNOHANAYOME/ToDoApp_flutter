5faee92995af0323c28f914fc33efd47e6e6de05
test <test@Kentas-MacBook-Pro.local>
2024-05-12 15:17:26 +0900

    Visibilityを使用した削除機能の追加

--- lib/main.dart
+++ lib/main.dart
@@ -29,12 +29,12 @@ class TodoListPage extends StatefulWidget {
 class _TodoListPageState extends State<TodoListPage> {
 
   List<String> todoList = [];
+  List<bool> isEnableList = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.menu),
         title: const Text('Todoリスト'),
         backgroundColor: Colors.orange,
         centerTitle: true,
@@ -53,15 +53,46 @@ class _TodoListPageState extends State<TodoListPage> {
             ),
           ],
       ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              title: const Text('メニュー1'),
+              onTap: () {
+                
+              },
+            ),
+            ListTile(
+              title: const Text('メニュー2'),
+              onTap: () {
+                
+              },
+            ),
+            ListTile(
+              title: const Text('メニュー3'),
+              onTap: () {
+                
+              },
+            )
+          ],
+        ),
+      ),
       body: Center(
-        //builderメソッドでデータを元に表示
         child: ListView.builder(
           itemCount: todoList.length,
           itemBuilder: (context, index) {
-            return Card(
-              child: ListTile(
-                title: Text(todoList[index]),
-              ),
+            return Visibility(
+              visible: isEnableList[index],
+              child: Card(
+                child: ListTile(
+                  title: Text(todoList[index]),
+                  onTap: (){
+                    setState(() {
+                      isEnableList[index] = false;
+                    });
+                  },
+                ),
+              )
             );
           },
         )
@@ -80,6 +111,7 @@ class _TodoListPageState extends State<TodoListPage> {
           if (newListText != null) {
 
             setState(() {
+              isEnableList.add(true);
               todoList.add(newListText);
             });
           }
@@ -157,117 +189,3 @@ class _TodoAddPageState extends State<TodoAddPage> {
     );
   }
 }
-// class TodoListPage extends StatelessWidget {
-//   const TodoListPage({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         leading: Icon(Icons.menu),
-//         title: const Text('Todoリスト'),
-//         backgroundColor: Colors.orange,
-//         centerTitle: true,
-//         actions: <Widget>[
-//             IconButton(
-//               icon: Icon(Icons.face, color: Colors.white,),
-//               onPressed: () {},
-//             ),
-//             IconButton(
-//               icon: Icon(Icons.email, color: Colors.white,),
-//               onPressed: () {},
-//             ),
-//             IconButton(
-//               icon: Icon(Icons.favorite, color: Colors.white,),
-//               onPressed: () {},
-//             ),
-//           ],
-//       ),
-//       body: Center(
-//         child: ListView(
-//           children: <Widget>[
-//             Card(
-//               child: ListTile(
-//                 title: Text('タマネギ'),
-//               ),
-//             )
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () async {
-//           //ルーティング処理になってる
-//           final newListText = await Navigator.of(context).push(
-//             MaterialPageRoute(builder: (context) {
-//               // class(ページ)を返す
-//               return TodoAddPage();
-//             }),
-//           );
-//           if (newListText != null) {
-//             // キャンセルした場合は newListText が null となるので注意
-//           }
-//         },
-//         child: Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
-
-// class TodoAddPage extends StatelessWidget {
-//   const TodoAddPage({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Todoリスト'),
-//         centerTitle: true,
-//         backgroundColor: Colors.orange,
-//         actions: <Widget>[
-//           IconButton(
-//             onPressed: (){},
-//             icon: Icon(Icons.face, color: Colors.white,)
-//           ),
-//           IconButton(
-//             onPressed: (){}, 
-//             icon: Icon(Icons.mail, color: Colors.white,)
-//           ),
-//           IconButton(
-//             onPressed: (){}, 
-//             icon: Icon(Icons.favorite, color: Colors.white)
-//           )
-//         ],
-//       ),
-//       body: Container(
-//         padding: EdgeInsets.all(64),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             TextField(),
-//             const SizedBox(height: 8,),
-//             Container(
-//               width: double.infinity,
-//               child: ElevatedButton(
-//                 style: ElevatedButton.styleFrom(
-//                   backgroundColor: Colors.orange
-//                 ),
-//                 onPressed: (){},
-//                 child: Text('リスト追加', style: TextStyle(color: Colors.white),),
-//               )
-//             ),
-//             const SizedBox(height: 8,),
-//             Container(
-//               width: double.infinity,
-//               child: TextButton(
-//                 onPressed: () {
-//                   Navigator.of(context).pop();
-//                 },
-//                 child: Text('キャンセル')
-//               ),
-//             )
-//           ],
-//         ),
-//       )
-//     );
-//   }
-// }
